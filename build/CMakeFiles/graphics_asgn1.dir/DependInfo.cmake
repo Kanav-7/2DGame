ca6d545ee8254ec9ae642728dec337e38944c39a
Kanav Gupta <kanavgupta0711@gmail.com>
2018-02-02 01:13:54 +0530

    Makefile added

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/ball.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/color.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/input.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/main.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/nonedit.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/other_handlers.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/rectangle.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/rectangle.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/semi.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/semi.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/timer.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/src/triangle.cpp" "/home/kanav/sem-4/Graphics/Assignment2/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
